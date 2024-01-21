5be223e15050d5c73b0817464d42e010d441c715
dmitriydmx <dmitriydmx@mail.ru>
2024-01-21 17:22:34 +0900

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 import Game, { 
   GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame 
 } from './game';
 const game = new Game();
 game.start();
-=======
->>>>>>> c0740d0e79f64acac336cbc2e5e4d090fe6416c9
+
 console.log('app worked')
